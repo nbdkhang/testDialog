79f5bec8bdd8c44837214bc7e106e03f0df111f3
nbdkhang <nbdkhang@gmail.com>
2022-02-28 22:29:09 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,99 @@
+import logo from "./logo.svg";
+import React, { useState } from "react";
+import { makeStyles } from "@mui/styles";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Alert from "@mui/material/Alert";
+import Collapse from '@mui/material/Collapse';
+import "./App.css";
+
+const useStyles = makeStyles({
+  dialog_form: {
+    width: "500px",
+  },
+  input: {
+    margin: "10px",
+    padding: "20px",
+  },
+});
+function App() {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [openSuccess, setOpenSuccess] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpenSuccess = () => {
+    setOpenSuccess(true);
+  };
+  const handleSubmit = () => {
+    handleOpenSuccess();
+    handleClose();
+    console.log(username);
+    console.log(email);
+  };
+
+  return (
+    <div className="App">
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open Dialog
+      </Button>
+      <Dialog
+        className={classes.dialog_form}
+        open={open}
+        onClose={handleClose}
+        fullWidth
+        maxWidth="md"
+      >
+        <DialogContent>
+          <TextField
+            className="input"
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Username"
+            type="text"
+            fullWidth
+            variant="standard"
+            defaultValue={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            className="input"
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+            defaultValue={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleSubmit}>Submit</Button>
+        </DialogActions>
+      </Dialog>
+
+
+      <Collapse in={openSuccess}>
+        <Alert severity="success">
+          This is a success alert — check it out!
+        </Alert>
+      </Collapse>
+    </div>
+  );
+}
+
+export default App;
